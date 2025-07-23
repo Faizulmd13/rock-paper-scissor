a1a0506bd50be327956a2c1f287e74e9fd6d32d9
Faizul <faizulmd1306@gmail.com>
2025-07-23 15:27:59 +0530

    Define playRound function

--- script.js
+++ script.js
@@ -13,10 +13,51 @@ let getComputerChoice = function () {
   }
 };
 
-console.log(getComputerChoice());
+// console.log(getComputerChoice());
 
 let getHumanChoice = () => {
   return prompt("Enter your choice").toLowerCase();
 };
 
-console.log(getHumanChoice());
+// console.log(getHumanChoice());
+
+let humanScore = (computerScore = 0);
+
+let playRound = (humanChoice, computerChoice) => {
+  if (humanChoice === "rock") {
+    if (computerChoice === "rock") {
+      console.log(`It's a tie. ${humanChoice} ties with ${computerChoice}`);
+    } else if (computerChoice === "scissor") {
+      console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+      humanScore += 1;
+    } else {
+      console.log(`You lose! ${humanChoice} lost against ${computerChoice}`);
+      computerScore += 1;
+    }
+  } else if (humanChoice === "paper") {
+    if (computerChoice === "paper") {
+      console.log(`It's a tie. ${humanChoice} ties with ${computerChoice}`);
+    } else if (computerChoice === "rock") {
+      console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+      humanScore += 1;
+    } else {
+      console.log(`You lose! ${humanChoice} lost against ${computerChoice}`);
+      computerScore += 1;
+    }
+  } else if (humanChoice === "scissor") {
+    if (computerChoice === "scissor") {
+      console.log(`It's a tie. ${humanChoice} ties with ${computerChoice}`);
+    } else if (computerChoice === "paper") {
+      console.log(`You won! ${humanChoice} beats ${computerChoice}`);
+      humanScore += 1;
+    } else {
+      console.log(`You lose! ${humanChoice} lost against ${computerChoice}`);
+      computerScore += 1;
+    }
+  } else {
+    console.log(`Your choice ${humanChoice} is invalid`);
+  }
+  console.log(`Your Score: ${humanScore}, Computer Score: ${computerScore}`);
+};
+
+playRound(getHumanChoice(), getComputerChoice());
